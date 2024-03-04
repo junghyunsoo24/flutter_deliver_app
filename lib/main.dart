009bd587c7c55f71c31de743c7502af275e454ae
hyunsoo24 <sunni303356@naver.com>
2024-03-04 15:42:54 +0900

    feat: 메인 화면 이메일, 비밀번호 입력 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'component/custom_text_form_field.dart';
+
 void main() {
   runApp(
-    const MyApp(),
+    MyApp(),
   );
 }
 
@@ -12,10 +14,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: Container(),
-        )
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+              hintText: '이메일을 입력해주세요.',
+              onChanged: (String value) {},
+            ),
+            CustomTextFormField(
+              hintText: '비밀번호를 입력해주세요.',
+              onChanged: (String value) {},
+              obscureText: true,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
