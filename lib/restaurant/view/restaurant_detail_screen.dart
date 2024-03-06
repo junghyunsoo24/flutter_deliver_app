55b907a9fbf2aca227eb652f5a19c0a592ea64b4
hyunsoo24 <sunni303356@naver.com>
2024-03-06 21:00:39 +0900

    feat: 디테일 클래스 금액,별점등 더 세부적인것들 작성

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 import '../../common/layout/default_layout.dart';
+import '../../product/component/product_card.dart';
 import '../component/restaurant_card.dart';
 
 class RestaurantDetailScreen extends StatelessWidget {
@@ -17,13 +18,17 @@ class RestaurantDetailScreen extends StatelessWidget {
                 'asset/img/food/ddeok_bok_gi.jpg'
             ),
             name: '불타는 떡볶이',
-            tags: ['떡볶이', '맛있음', '치즈'],
+            tags: const ['떡볶이', '맛있음', '치즈'],
             ratingsCount: 100,
             deliveryTime: 30,
             deliveryFee: 3000,
             ratings: 4.76,
             isDetail: true,
             detail: '맛있는 떡볶이',
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: ProductCard(),
           ),
         ],
       ),
