3c7994f57783875fe36bcdf6cbd368716c921147
hyunsoo24 <sunni303356@naver.com>
2024-03-11 00:52:29 +0900

    feat: 리뷰 바디 적용, 리뷰 repository 초기 작업

--- lib/rating/component/rating_card.dart
+++ lib/rating/component/rating_card.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
 
 import '../../common/const/colors.dart';
 
@@ -40,8 +40,17 @@ class RatingCard extends StatelessWidget {
           email: email,
           rating: rating,
         ),
-        _Body(),
-        _Images(),
+        const SizedBox(height: 8.0),
+        _Body(
+          content: content,
+        ),
+        if (images.length > 0)
+          SizedBox(
+            height: 100,
+            child: _Images(
+              images: images,
+            ),
+          ),
       ],
     );
   }
@@ -92,19 +101,53 @@ class _Header extends StatelessWidget {
 }
 
 class _Body extends StatelessWidget {
-  const _Body({Key? key}) : super(key: key);
+  final String content;
+
+  const _Body({
+    required this.content,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Flexible(
+          child: Text(
+            content,
+            style: const TextStyle(
+              color: BODY_TEXT_COLOR,
+              fontSize: 14.0,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
 
 class _Images extends StatelessWidget {
-  const _Images({Key? key}) : super(key: key);
+  final List<Image> images;
+
+  const _Images({required this.images, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: images
+          .mapIndexed(
+            (index, e) => Padding(
+          padding: EdgeInsets.only(
+            right: index == images.length - 1 ? 0 : 16.0,
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: e,
+          ),
+        ),
+      )
+          .toList(),
+    );
   }
 }
---
+++ lib/restaurant/repository/restaurant_rating_repository.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart' hide Headers;
+import '../../common/const/data.dart';
+import '../../common/dio/dio.dart';
+import '../../common/model/cursor_pagination_model.dart';
+import '../../common/model/pagination_params.dart';
+import '../../rating/model/rating_model.dart';
+
+part 'restaurant_rating_repository.g.dart';
+
+final restaurantRatingRepositoryProvider = Provider.family<
+    RestaurantRatingRepository,
+    String
+>((ref, id){
+  final dio = ref.watch(dioProvider);
+
+  return RestaurantRatingRepository(dio, baseUrl: 'http://$ip/restaurant/$id/rating');
+});
+
+// http://ip/restaurant/:rid/rating
+@RestApi()
+abstract class RestaurantRatingRepository {
+  factory RestaurantRatingRepository(Dio dio, {String baseUrl}) =
+  _RestaurantRatingRepository;
+
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<CursorPagination<RatingModel>> paginate({
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
+}
