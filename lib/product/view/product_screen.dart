534d8d44700dd47bbaad28685d2faf642f95e71f
hyunsoo24 <sunni303356@naver.com>
2024-03-13 06:32:07 +0900

    feat: 음식 버튼 누를때도 캐시 이용해 로딩후 나오도록 작성

--- lib/product/view/product_screen.dart
+++ lib/product/view/product_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import '../../common/component/pagination_list.view.dart';
+import '../../restaurant/view/restaurant_detail_screen.dart';
 import '../component/product_card.dart';
 import '../model/product_model.dart';
 import '../provider/product_provider.dart';
@@ -11,9 +13,20 @@ class ProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return PaginationListView<ProductModel>(
       provider: productProvider,
-      itemBuilder: <ProductModel>(_, index, model){
-        return ProductCard.fromProductModel(
-          model: model,
+      itemBuilder: <ProductModel>(_, index, model) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => RestaurantDetailScreen(
+                  id: model.restaurant.id,
+                ),
+              ),
+            );
+          },
+          child: ProductCard.fromProductModel(
+            model: model,
+          ),
         );
       },
     );
--- lib/restaurant/provider/restaurant_provider.dart
+++ lib/restaurant/provider/restaurant_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:collection/collection.dart';
 import '../../common/model/cursor_pagination_model.dart';
-import '../../common/model/pagination_params.dart';
 import '../../common/provider/pagination_provider.dart';
 import '../model/restaurant_model.dart';
 import '../repository/restaurant_repository.dart';
@@ -13,7 +13,7 @@ Provider.family<RestaurantModel?, String>((ref, id) {
     return null;
   }
 
-  return state.data.firstWhere((element) => element.id == id);
+  return state.data.firstWhereOrNull((element) => element.id == id);
 });
 
 final restaurantProvider =
@@ -51,16 +51,31 @@ class RestaurantStateNotifier
 
     final resp = await repository.getRestaurantDetail(id: id);
 
-    // [RestaurantMode(1), RestaurantModel(2), RestaurantModel(3)]
-    // id : 2인 친구를 Detail모델을 가져와라
-    // getDetail(id: 2);
-    // [RestaurantMode(1), RestaurantDetailModel(2), RestaurantModel(3)]
-    state = pState.copyWith(
-      data: pState.data
-          .map<RestaurantModel>(
-            (e) => e.id == id ? resp : e,
-      )
-          .toList(),
-    );
+    // [RestaurantModel(1), RestaurantModel(2), RestaurantModel(3)]
+    // 요청 id: 10
+    // list.where((e) => e.id == 10)) 데이터 X
+    // 데이터가 없을때는 그냥 캐시의 끝에다가 데이터를 추가해버린다.
+    // [RestaurantModel(1), RestaurantModel(2), RestaurantModel(3),
+    // RestaurantDetailModel(10)]
+    if(pState.data.where((e) => e.id == id).isEmpty){
+      state = pState.copyWith(
+        data: <RestaurantModel>[
+          ...pState.data,
+          resp,
+        ],
+      );
+    }else{
+      // [RestaurantModel(1), RestaurantModel(2), RestaurantModel(3)]
+      // id : 2인 친구를 Detail모델을 가져와라
+      // getDetail(id: 2);
+      // [RestaurantModel(1), RestaurantDetailModel(2), RestaurantModel(3)]
+      state = pState.copyWith(
+        data: pState.data
+            .map<RestaurantModel>(
+              (e) => e.id == id ? resp : e,
+        )
+            .toList(),
+      );
+    }
   }
 }
